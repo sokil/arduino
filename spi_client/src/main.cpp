cfcc3fa0dbdc6993012f5e1c4dc30faffe3b4dea
Dmytro Sokil <dmytro.sokil@gmail.com>
2025-01-11 23:13:27 +0200

    fix

--- spi_client/src/main.cpp
+++ spi_client/src/main.cpp
@@ -3,6 +3,17 @@
 volatile byte receivedData;
 volatile bool dataReceived = false;
 
+unsigned long now = 0;
+unsigned long prev_now = 0;
+
+int sck_state = -1;
+int cs_state = -1;
+int mosi_state = -1;
+
+int current_cs_state = -1;
+int current_sck_state = -1;
+int current_mosi_state = -1;
+
 // #define PIN_SPI_SS    (10)
 // #define PIN_SPI_MOSI  (11)
 // #define PIN_SPI_MISO  (12)
@@ -21,7 +32,7 @@ void setup() {
   // Attach SPI interrupt
   SPI.attachInterrupt();
 
-  Serial.print("SPI Started");
+  Serial.println("SPI Started");
 }
 
 ISR(SPI_STC_vect) {
@@ -31,16 +42,50 @@ ISR(SPI_STC_vect) {
 }
 
 void loop() {
-  // If data has been received
+  // handle pin states
+  current_cs_state = digitalRead(SS);
+  current_sck_state = digitalRead(SCK);
+  current_mosi_state = digitalRead(MOSI);
+
+  if (
+    current_cs_state != cs_state 
+    || current_sck_state != sck_state 
+    || current_mosi_state != mosi_state
+  ) {
+      prev_now = now;
+      now = millis();
+
+      if (current_cs_state != cs_state ) {
+        cs_state = current_cs_state;
+      }
+
+      if (current_sck_state != sck_state) {
+        sck_state = current_sck_state;
+      }
+
+      if (current_mosi_state != mosi_state) {
+        mosi_state = current_mosi_state;
+      }
+
+      Serial.print(cs_state == LOW ? "CS=LOW,  " : "CS=HIGH, ");
+      Serial.print(sck_state == LOW ? "SCK=LOW,  " : "SCK=HIGH, ");
+      Serial.print(mosi_state == LOW ? "MOSI=LOW,  " : "MOSI=HIGH, ");
+      Serial.print("Delay=");
+      Serial.println(now - prev_now);
+  }
+
+  // print data byte
   if (dataReceived) {
-    // Print the received data in hex format
-    Serial.print("Received: 0x");
+    Serial.print("0x");
     if (receivedData < 0x10) {
-      Serial.print("0"); // Add leading zero for single hex digit
+      Serial.print("0");
     }
     Serial.println(receivedData, HEX);
+    Serial.println("");
 
-    // Reset the flag
     dataReceived = false;
   }
+
+  // wait all ports to switch
+  // delay(2);
 }
