98ac1a29606f93a91986eb9e84e302ee57e0a6c6
Dmytro Sokil <dmytro.sokil@gmail.com>
2025-02-24 21:57:55 +0200

    fix

---
+++ RX5808/src/main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+#define RSSI_PIN 25
+
+void setup() {
+  Serial.begin(9600);
+
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+
+  display.clearDisplay();
+
+}
+
+void loop() {
+  int rssiRaw = analogRead(RSSI_PIN);
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(40, 25); 
+  display.print(rssiRaw);
+  display.display();
+
+  Serial.println(rssiRaw);
+  delay(500);
+}
